ba3026d72dd62ae77770ce4741eff173f64c8ea6
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-08-23 19:07:34 +0900

    update build script

--- bin/build.sh
+++ bin/build.sh
@@ -8,13 +8,13 @@ mkdir -p .cache
 curl https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip -o .cache/font.zip
 unzip .cache/font.zip -d .cache/
 
-mkdir -p "Noto Sans CJK JP Bold"
-$(npm bin)/build-glyphs .cache/NotoSansCJKjp-Bold.otf "Noto Sans CJK JP Bold"
+mkdir -p "public/Noto Sans CJK JP Bold"
+$(npm bin)/build-glyphs .cache/NotoSansCJKjp-Bold.otf "public/Noto Sans CJK JP Bold"
 
-mkdir -p "Noto Sans CJK JP Regular"
-$(npm bin)/build-glyphs .cache/NotoSansCJKjp-Regular.otf "Noto Sans CJK JP Regular"
+mkdir -p "public/Noto Sans CJK JP Regular"
+$(npm bin)/build-glyphs .cache/NotoSansCJKjp-Regular.otf "public/Noto Sans CJK JP Regular"
 
 mkdir -p "Noto Sans CJK JP Light"
-$(npm bin)/build-glyphs .cache/NotoSansCJKjp-Light.otf "Noto Sans CJK JP Light"
+$(npm bin)/build-glyphs .cache/NotoSansCJKjp-Light.otf "public/Noto Sans CJK JP Light"
 
-rm -fr "Noto Sans Regular" && cp -fr "Noto Sans CJK JP Regular" "Noto Sans Regular"
+rm -fr "public/Noto Sans Regular" && cp -fr "public/Noto Sans CJK JP Regular" "public/Noto Sans Regular"
