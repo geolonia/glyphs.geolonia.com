f49a3da59b24cb34f07b1fc00573018f7acc2fe6
naoki <n.globe.us@gmail.com>
2022-05-31 17:15:53 +0900

    add Noto Universal

--- bin/build.sh
+++ bin/build.sh
@@ -5,16 +5,19 @@ set -ex
 rm -fr .cache
 mkdir -p .cache
 
-curl https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip -o .cache/font.zip
-unzip .cache/font.zip -d .cache/
+curl https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip -o .cache/NotoSansCJKjp.zip
+curl -L https://github.com/satbyy/go-noto-universal/releases/download/v5.1/GoNotoCurrent.ttf -o .cache/GoNotoCurrent.ttf
+
+unzip .cache/NotoSansCJKjp.zip -d .cache/
 
 mkdir -p "public/Noto Sans CJK JP Bold"
 $(npm bin)/build-glyphs .cache/NotoSansCJKjp-Bold.otf "public/Noto Sans CJK JP Bold"
 
+mkdir -p "public/Noto Sans CJK JP Light"
+$(npm bin)/build-glyphs .cache/NotoSansCJKjp-Light.otf "public/Noto Sans CJK JP Light"
+
 mkdir -p "public/Noto Sans CJK JP Regular"
 $(npm bin)/build-glyphs .cache/NotoSansCJKjp-Regular.otf "public/Noto Sans CJK JP Regular"
 
-mkdir -p "Noto Sans CJK JP Light"
-$(npm bin)/build-glyphs .cache/NotoSansCJKjp-Light.otf "public/Noto Sans CJK JP Light"
-
-rm -fr "public/Noto Sans Regular" && cp -fr "public/Noto Sans CJK JP Regular" "public/Noto Sans Regular"
+mkdir -p "public/Noto Sans Regular"
+$(npm bin)/build-glyphs .cache/GoNotoCurrent.ttf "public/Noto Sans Regular"
